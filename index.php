80e168859485eeb2b01e9fbe26cd793d10fde500
rellonjessrel <35474756+rellonjessrel@users.noreply.github.com>
2019-01-19 04:23:05 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>use diagram</head>
+<body>
+<img src="use.jpg.jpg"> <p> This use diagram shows the restriction of the user the admin and the 3rd party</p>
+<p>this loterry online shows the harmony to each other the flow of the system to easy to undertand by others </p>
+<p> it is easy way to understand  to us so that by doing our project we have a guide it is a big help for us to easy to develop</p>
+<p>user can access limited tables while admin can access all table for him to monitor his system if what is the status</p>
+<p>To avoid hacking the admin should access all table if their were anusual action or activity in thier application </p>
+<p> it is just simple but  hard to do and nto develop</p>
+</body>
+</html>
